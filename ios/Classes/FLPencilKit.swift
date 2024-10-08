750900eb81a106849e3ce0b2c90e5894d88a5a28
PaulPopTudor <162346806+paultpop@users.noreply.github.com>
2024-10-08 21:30:02 +0300

    Update FLPencilKit.swift

--- ios/Classes/FLPencilKit.swift
+++ ios/Classes/FLPencilKit.swift
@@ -526,6 +526,7 @@ extension UIColor {
     let alpha = Double((hex >> 24) & 0xFF) / 255
     let red = Double((hex >> 16) & 0xFF) / 255
     let green = Double((hex >> 8) & 0xFF) / 255
+    
     let blue = Double((hex >> 0) & 0xFF) / 255
 
     self.init(red: red, green: green, blue: blue, alpha: alpha)
